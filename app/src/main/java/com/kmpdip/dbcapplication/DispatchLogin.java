cf07c9216e9d5953751394989ea1b9d0791d79b3
Seichis <koumou276@gmail.com>
2015-10-20 20:11:40 +0200

    Initial commit

---
+++ app/src/main/java/com/kmpdip/dbcapplication/DispatchLogin.java
@@ -0,0 +1,14 @@
+package com.kmpdip.dbcapplication;
+
+import com.parse.ui.ParseLoginDispatchActivity;
+
+/**
+ * Launcher activity. When the user is not logged in he is redirected to the login screen else he is redirected in the MainActivity
+ */
+public class DispatchLogin extends ParseLoginDispatchActivity {
+
+    @Override
+    protected Class<?> getTargetClass() {
+        return MainActivity.class;
+    }
+}
---
+++ app/src/main/java/com/kmpdip/dbcapplication/MyApplication.java
@@ -0,0 +1,25 @@
+package com.kmpdip.dbcapplication;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+
+/**
+ * Created by tRiF on 28/7/2015.
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_key));
+
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+
+        ParseFacebookUtils.initialize(this);
+    }
+}
